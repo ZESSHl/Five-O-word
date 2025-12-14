6e30ff6d8202e9765c30d7f7be5bfab49fd41520
Zesshi <koojefferson@gmail.com>
2025-12-15 02:39:55 +0700

    Add files via upload

---
+++ game_easy.py
@@ -0,0 +1,122 @@
+import time
+import sys
+from word_check import check_words
+
+def definer(secret, guess):
+    result = ["absent"] * len(secret) # makes the guessed word into separate characters like ["absent", "absent"] depending on how long the word is
+    secret_list = list(secret)
+
+    # checks for characters that are correct place
+    for i, ch in enumerate(guess):
+        if ch == secret[i]:
+            result[i] = "correct"
+            secret_list[i] = None
+
+    # checks for character in word but not in place
+    for i, ch in enumerate(guess):
+        if result[i] == "correct":
+            continue
+        if ch in secret_list:
+            result[i] = "present"
+            secret_list[secret_list.index(ch)] = None
+
+    return result
+
+def arranger(guess, feedback):
+    out = []
+    for ch, status in zip(guess, feedback):
+        if status == "correct":
+            out.append(f"[{ch}]")   # for word sin correct spot in word
+        elif status == "present":
+            out.append(f"({ch})")   # for characters in word but in the wrong spot
+        else:
+            out.append(f" {ch} ")   # for characters not in word
+    print(" ".join(out))
+
+# utilities? 
+
+def start(words):
+    secret = words.random_word()
+    max_try = 0 
+    start_time = time.time()
+    print("Easy difficulty started!")
+    print("You have 7 tries.")
+
+
+    while max_try < 7:
+        player_guess = input("Input your word: ")
+        guess = player_guess.upper()
+
+        # require 5 letter words no more no less, does not include into tries
+        if len(guess) != len(secret):
+            print(f"Please enter a {len(secret)}-letter word.")
+            continue
+
+        if guess not in check_words:
+            print("Sorry, word is not in our dictionary")
+            print("Try again")
+            continue
+
+        if guess == secret:
+            end_time = time.time()
+            time_taken = end_time - start_time
+            total_seconds = int(time_taken)
+
+            minutes = total_seconds // 60
+            seconds = total_seconds % 60
+
+            print(f"Correct! time taken is {minutes} minutes and {seconds} seconds")
+            while True:
+                print(" ")
+                print("Play again? ")
+                print("1. Yes")
+                print("2. No")
+                print("3. Return to main menu")
+                decide = input()
+
+                if decide in ("1", "Yes", "yes"):
+                    return start(words)
+                elif decide in ("No", "no", "2"):
+                    sys.exit()
+                elif decide in ("Return", "Return to main", "Return to main menu", "3"):
+                    return
+                else:
+                    print("Invalid Input")
+
+        else:
+            # passes a wrong guess to the top def definer and arranger
+            feedback = definer(secret, guess)
+            arranger(guess, feedback)
+
+            max_try += 1
+            tries_left = 7 - max_try
+
+            if max_try < 7:
+                print(f"Incorrect, you have {tries_left} tries left")
+
+        if max_try == 7 and guess != secret:
+            end_time = time.time()
+            time_taken = end_time - start_time
+            total_seconds = int(time_taken)
+
+            minutes = total_seconds // 60
+            seconds = total_seconds % 60
+            print(f"Nice try. Time taken is {minutes} minutes and {seconds} seconds.")
+            print(f"The word is: {secret}")
+
+            while True:
+                print(" ")
+                print("Play again? ")
+                print("1. Yes")
+                print("2. No")
+                print("3. Return to main menu")
+                decide = input()
+
+                if decide in ("1", "Yes", "yes"):
+                    return start(words)
+                elif decide in ("No", "no", "2"):
+                    sys.exit()
+                elif decide in ("Return", "Return to main", "Return to main menu", "3"):
+                    return
+                else:
+                    print("Invalid Input")
---
+++ main.py
@@ -0,0 +1,36 @@
+import Word_list
+
+def main():
+    while True:
+        print("Welcome to Five O'word!")
+        print("A game of 1 word, 5 letters, limited tries")
+        print("A letter encased in [] means it's in the right place") #example: [A]
+        print("A letter encased in () means it's in the wrong spot") #example (A)
+        print("A word not encased means the letter is not in the word") #example A
+        print("1. Easy")
+        print("2. Medium")
+        print("3. Hard")
+        print("4. Exit Game")
+
+        choice = input("Enter number: ")
+
+        if choice == "1":
+            import game_easy as game
+            words = Word_list
+            game.start(words)
+        elif choice == "2":
+            import game_medium as game
+            words = Word_list
+            game.start(words)
+        elif choice == "3":
+            import game_hard as game
+            words = Word_list
+            game.start(words)
+        elif choice == "4":
+            print("Game Exited")
+            exit()
+        else:
+            print("Invalid Input")
+
+if __name__ == "__main__":
+    main()
